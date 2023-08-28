18407df87642281a8487e1ab7bc940341a9ea114
celsj <celsj@mcmaster.ca>
2023-08-28 11:38:23 -0400

    Added date picker, date component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 //import Header from './components/Header';
-import {CommentField, BinaryField} from './components/ImportHelper';
+import {CommentField, BinaryField, DateField} from './components/ImportHelper';
 
 function App() {
   return (
     <div className="App">
       <CommentField title="Comments" limit={500}/>
       <BinaryField title="Bowel Movement" name="bowel-movement"/>
+      <DateField title="Hello"/>
     </div>
   );
 }
--- src/components/SheetView/Fields/DateField.tsx
+++ src/components/SheetView/Fields/DateField.tsx
@@ -1,14 +1,29 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 interface DateProps {
     title: string;
 }
 
 const DateField: FC<DateProps> = ({ title }) => {
-    return (<>
-        <h1>{title}!</h1>
-    </>)
+    const [startDate, setDate] = useState(new Date());
+    
+    const selectDateHandler = (date: Date) => {
+        setDate(date)
+    }
+
+    return (
+    <div>
+        <h1>{title}</h1>
+        <DatePicker
+            dateFormat="yyyy/MM/dd"
+            selected={startDate}    
+            onChange={selectDateHandler}
+            todayButton={"Today"}/>
+    </div>
+    )
+    
 };
 
 export default DateField;
