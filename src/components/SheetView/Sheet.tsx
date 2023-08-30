b48cfbc5e1ff0a45570243ebd8cdb379f78d9648
celsj <celsj@mcmaster.ca>
2023-08-30 14:27:17 -0400

    Added flex display styling

--- src/components/SheetView/Sheet.tsx
+++ src/components/SheetView/Sheet.tsx
@@ -16,70 +16,97 @@ const Sheet: FC<SheetProps> = ({ title, name }) => {
     ];
 
     return (
-    <div className="Sheet">
-        <div className="sheet-wrapper">
-            <div>
-                <span>
-                    Mike Cels
+    <div className="sheet">
+        <div className="sheet-container">
+            <div className="sheet-head-wrapper">
+                <div className="sheet-head-left-wrapper">
+                    <div>
+                        <span>
+                            Mike Cels
+                            <br></br>
+                            Medical Tracking
+                        </span>
+                    </div>
                     <br></br>
-                    Medical Tracking
-                </span>
+                    <div>
+                        <DateField title="DATE:"/>
+                    </div>
+                </div>
+                <div className="sheet-head-middle-wrapper">
+                    <div>
+
+                    </div>
+                </div>
+                <div className="sheet-head-right-wrapper">
+                    <div>
+                        <CommentField title="IMPORTANT NOTES" limit={500}/>
+                    </div>
+                </div>
             </div>
-            <div>
-                <CommentField title="IMPORTANT NOTES" limit={500}/>
+            <div className="sheet-box-container">
+                <div className="sheet-row-container">
+                    <div className="box-1-edaravone">
+                        <BinaryField title="Edaravone?" name="edaravone"/>
+                        <CommentField title="Comments" limit={500}/>
+                    </div>
+                    <div className="box-2-edaravone">
+                        <BinaryField title="Change in Edaravone Routine?" name="edaravone-change"/>
+                    </div>
+                    <div className="box-3-bipap">
+                        <BinaryField title="Previous Night Bipap?" name="bipap-night"/>
+                        <IntField title="Hours on Bipap Overnight"/>
+                    </div>
+                    <div className="box-4-bipap">
+                        <BinaryField title="Daytime Use of Bipap?" name="bipap-day" />
+                        <IntField title="Hours of Daytime Bipap"/>
+                    </div>
+                </div>
+                <div className="sheet-row-container">
+                    <div className="box-5-sleep">
+                        <CommentField title="Repositioned how often?" limit={50}/>
+                    </div>
+                    <div className="box-6-bipap">
+                        <CommentField title="Bipap Comments" limit={500}/>
+                    </div>
+                </div>
+                <div className="sheet-row-container">
+                    <div className="box-7-elimination">
+                        <BinaryField title="BM?" name="bowel-movement"/>
+                        <IntField title="Urine Output"/>
+                        <BinaryField title="Morning?" name="urine-morning"/>
+                        <DecimalField title="Daily Volume"/>
+                    </div>
+                    <div className="box-8-medication">
+                        <CommentField title="Change in RX?" limit={500}/>
+                        <CommentField title="Change in Routine?" limit={500}/>
+                    </div>
+                </div>
+                <div className="sheet-row-container">
+                    <div className="box-9-gtube">
+                        <BinaryField title='Morning \"Shake\" Via Pump?' name="morning-shake-via-pump"/>
+                        <CommentField title="Other?" limit={500}/>
+                    </div>
+                    <div className="box-10-dinner">
+                        <CommentField title="Dinner/Oral Feeding Notes" limit={500}/>
+                    </div>
+                </div>
+                <div className="sheet-row-container">
+                    <div className="box-11-pain-discomfort">
+                        <BinaryField title="Pain/Discomfort?" name="pain-discomfort"/>
+                        <DropdownField title="Source" options={dropdownOptions}/>
+                        <CommentField title="Notes" limit={500}/>
+                    </div>
+                    <div className="box-12-choking-gagging">
+                        <BinaryField title="Suction Machine?" name="suction-machine"/>
+                    </div>
+                </div>
             </div>
-            <div>
-                <DateField title="DATE:"/>
+            <div className="sheet-foot-wrapper">
+                <div>
+                    <CommentField title="NOTES, COMMENTS, AREAS OF CONCERN OR FOLLOWUP" limit={1000}/>
+                </div>
             </div>
-            <div>
-                <BinaryField title="Edaravone?" name="edaravone"/>
-                <CommentField title="Comments" limit={500}/>
-            </div>
-            <div>
-                <BinaryField title="Change in Edaravone Routine?" name="edaravone-change"/>
-            </div>
-            <div>
-                <BinaryField title="Previous Night Bipap?" name="bipap-night"/>
-                <IntField title="Hours on Bipap Overnight"/>
-            </div>
-            <div>
-                <BinaryField title="Daytime Use of Bipap?" name="bipap-day" />
-                <IntField title="Hours of Daytime Bipap"/>
-            </div>
-            <div>
-                <CommentField title="Repositioned how often?" limit={50}/>
-            </div>
-            <div>
-                <CommentField title="Bipap Comments" limit={500}/>
-            </div>
-            <div>
-                <BinaryField title="BW?" name="bowel-movement"/>
-                <IntField title="Urine Output"/>
-                <BinaryField title="Morning?" name="urine-morning"/>
-                <DecimalField title="Daily Volume"/>
-            </div>
-            <div>
-                <CommentField title="Change in RX?" limit={500}/>
-                <CommentField title="Change in Routine?" limit={500}/>
-            </div>
-            <div>
-                <BinaryField title='Morning \"Shake\" Via Pump?' name="morning-shake-via-pump"/>
-                <CommentField title="Other?" limit={500}/>
-            </div>
-            <div>
-                <CommentField title="Dinner/Oral Feeding Notes" limit={500}/>
-            </div>
-            <div>
-                <BinaryField title="Pain/Discomfort?" name="pain-discomfort"/>
-                <DropdownField title="Source" options={dropdownOptions}/>
-                <CommentField title="Notes" limit={500}/>
-            </div>
-            <div>
-                <BinaryField title="Suction Machine?" name="suction-machine"/>
-            </div>
-            <div>
-                <CommentField title="NOTES, COMMENTS, AREAS OF CONCERN OR FOLLOWUP" limit={1000}/>
-            </div>
+            
         </div>
     </div>
     )
